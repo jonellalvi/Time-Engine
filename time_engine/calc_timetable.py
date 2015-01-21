7e344a2b62e2c9fdd0fde3343e8880f03035ebb6
Jonell <jonellalvi@gmail.com>
2015-01-21 13:54:33 -0800

    got it working

--- time_engine/calc_timetable.py
+++ time_engine/calc_timetable.py
@@ -5,18 +5,18 @@ import calendar
 # Take that input and process it here to figure out the dates
 # Problem -- what format is the data?
 # {
-            #   'has_thursday': False,
-            #   'name': u'Spam',
-            #   'color': u'yellow',
-            #   'start_time': datetime.time(4, 20),
-            #   'has_tuesday': False,
-            #   'has_saturday': False,
-            #   'has_wednesday': True,
-            #   'lesson_count': 14,
-            #   'has_monday': True,
-            #   'has_friday': True,
-            #   'start_date': datetime.date(2000, 3, 25)
-            # }
+# 'has_thursday': False,
+#   'name': u'Spam',
+#   'color': u'yellow',
+#   'start_time': datetime.time(4, 20),
+#   'has_tuesday': False,
+#   'has_saturday': False,
+#   'has_wednesday': True,
+#   'lesson_count': 14,
+#   'has_monday': True,
+#   'has_friday': True,
+#   'start_date': datetime.date(2000, 3, 25)
+# }
 # Problem --> what output do I want to create?
 # probably a list of dates and times?
 
@@ -32,9 +32,9 @@ form_data = {
     'has_wednesday': True,
     'lesson_count': 20,
     'has_sunday': False,
-    'has_monday': True,
+    'has_monday': False,
     'has_friday': True,
-    'start_date': datetime.date(2010, 5, 3)
+    'start_date': datetime.date(2015, 2, 4)
 }
 
 #create an empty list that will hold the dates
@@ -80,6 +80,7 @@ def is_available():
     available_days.append(True if form_data.get('has_sunday') else False)
 
     return available_days
+
 # [True, False, True, False, True, False, False]
 is_available()
 
@@ -93,27 +94,52 @@ is_available()
 
 def get_timetable(lesson_count, start_date):
     dayDelta = datetime.timedelta(days=1)
-    current_date = start_date
+
     # goal is to make a list of dates for the lessons
     timetable = []
-    timetable.append(start_date)
+    available = is_available()
+    # figure out the first valid date:
+    if available[start_date.weekday()]:
+        # if avail, add to list
+        timetable.append(start_date)
+    # get the next date
+    next_date = start_date + datetime.timedelta(days=1)
+
+    print "The next day should be: ", start_date + datetime.timedelta(days=1)
     while lesson_count > 0:
         # need to start at startdate and loop through subsequent dates
         # if the date is avaialbe, add it to the list of dates
         # and decriment lesson_count
         # if the date is not available, jsut loop to the next one.
-        available = is_available()
-        if available[current_date.weekday()]:
-            timetable.append(current_date)
-            start_date += dayDelta
+        #available = is_available()
+        if available[next_date.weekday()]:
+            timetable.append(next_date)
+            next_date += datetime.timedelta(days=1)
             lesson_count -= 1
-
+        else:
+            #get the next date
+            next_date += datetime.timedelta(days=1)
     print timetable
+    date_strings = [dt.strftime("%A %B %d, %Y") for dt in timetable]
+    print date_strings
+
 
 get_timetable(lesson_count, start_date)
 
-# I need to write something to get the available days. hmmm
+
 # ---------------------------------------------------------------------
- 
+#
+#         # -- Add a day to the last date
+#         next_date = timetable[-1] + datetime.timedelta(days=1)
+#
+#         if date_is_available(next_date, valid_days):
+#             timetable.append(next_date)
+#             lesson_count -= 1
+#
+#         days_scanned += 1
+#         if days_scanned > 365 * 2:
+#         # return error that we looked 2 years into the future & could not schedule all the dates...
+#             pass
+#     return timetable
 
 
