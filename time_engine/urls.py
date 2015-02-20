2840bd0cca80314920029e32bcd977461a27c536
Jonell Alvi <jonellalvi@gmail.com>
2015-02-19 21:41:16 -0800

    deployed; added styles

--- time_engine/urls.py
+++ time_engine/urls.py
@@ -5,7 +5,7 @@ urlpatterns = patterns('',
 
         url(r'^register/', views.register, name='register'),
         url(r'^login/', views.user_login, name='login'),
-        url(r'^logout', views.user_logout, name='index'),
+        url(r'^logout/', views.user_logout, name='index'),
         url(r'^$', views.index, name='index'),
         url(r'^delete_timetable/', views.delete_timetable, name='delete_timetable'),
         url(r'^ajax/', views.ajax, name='ajax'),
